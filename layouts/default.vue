c8ea90f62e0eb65e231c72849070c1ee16f62e5c
mate2000 <33140312+mate2000@users.noreply.github.com>
2020-10-09 22:21:44 -0500

    Formulario basico,usuarios

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,43 @@
 <template>
   <!-- Layout de barra de navegación para las páginas -->
   <div>
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Menú</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item :to="item.route" v-for="item in items_menu" :key="item.id">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
+    <v-app>
+      <v-navigation-drawer v-model="drawer" app>
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title style="padding: 5px">{{
-              item.title
-            }}</v-list-item-title>
+            <v-list-item-title>Menú</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
 
-    <v-app-bar color="#0096b8" dense
-      ><v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon
-      ><v-toolbar-title>El Taller</v-toolbar-title><v-spacer></v-spacer
-      ><v-btn @click="logout()">Salir</v-btn></v-app-bar
-    ><v-main
-      ><v-container> <nuxt /> </v-container
-    ></v-main>
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            :to="item.route"
+            v-for="item in items_menu"
+            :key="item.id"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title style="padding: 5px">{{
+                item.title
+              }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar color="#0096b8" dense
+        ><v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon
+        ><v-toolbar-title>El Taller</v-toolbar-title><v-spacer></v-spacer
+        ><v-btn @click="logout()">Salir</v-btn></v-app-bar
+      ><v-main
+        ><v-container> <nuxt /> </v-container
+      ></v-main>
+    </v-app>
   </div>
 </template>
 <script>
---
+++ pages/register.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Nombre completo"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Documento de identidad"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Contraseña"
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="select"
+        :items="items"
+        :rules="[(v) => !!v || 'Item is required']"
+        label="Entidad"
+        required
+      ></v-select>
+
+      <v-select
+        v-model="select"
+        :items="items"
+        :rules="[(v) => !!v || 'Item is required']"
+        label="Rol"
+        required
+      ></v-select>
+
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[(v) => !!v || 'You must agree to continue!']"
+        label="Do you agree?"
+        required
+      ></v-checkbox>
+
+      <v-btn :disabled="!valid" class="mr-4" @click="validate">
+        Validate
+      </v-btn>
+
+      <v-btn class="mr-4" @click="reset"> Reset Form </v-btn>
+
+      <v-btn @click="resetValidation"> Reset Validation </v-btn>
+    </v-form>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 40) || "",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
+
