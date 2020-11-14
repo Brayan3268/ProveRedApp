e7f8cff22fcb55b1f771e14b0d6091f563317fc2
mate2000 <33140312+mate2000@users.noreply.github.com>
2020-11-14 14:03:15 -0500

    Creacion de usuario y de proveedor

--- pages/formProvider.vue
+++ pages/formProvider.vue
@@ -78,9 +78,7 @@
 export default {
   layout: "blank",
   beforeMount() {
-    this.loadUsers();
-    this.loadUser(this.users[this.users.length - 1]);
-    console.log(this.users);
+    this.loadUser();
   },
   beforeUpdate() {
     this.loadUsers();
@@ -120,40 +118,31 @@ export default {
   }),
 
   methods: {
-    loadUsers() {
-      
-      // let users = localStorage.getItem("users");
-      // if (users != null) {
-      //   this.users = JSON.parse(users);
-      // }
-      // console.log(this.users);
-
-      // let userProviders = localStorage.getItem("userProviders");
-      // if (userProviders != null) {
-      //   this.userProviders = JSON.parse(userProviders);
-      // }
-    },
-
-    loadUser(user) {
-      this.userProvider.id = user.id;
+    loadUser() {
+      let id = sessionStorage.getItem("idProvider");
+      this.userProvider.id = id;
     },
     finishFormProvider() {
       // this.userProvider.id = this.users[this.users.length - 1];
       if (this.$refs.formProvider.validate() && this.formProvider) {
-        let exist = this.userProviders.find(
-          (x) => x.id == this.userProvider.id
-        );
-        if (exist == undefined) {
-          this.userProviders.push(this.userProvider);
-          localStorage.setItem(
-            "userProviders",
-            JSON.stringify(this.userProviders)
-          );
-          alert("Proveedor registrado correctamete");
-          this.$router.push("/");
-        } else {
-          alert("La persona que intenda crear ya existe");
-        }
+        const url = "http://localhost:3001/api/v2/providers";
+        let data = {};
+
+        data.idUser = this.userProvider.id;
+        data.companyName = this.userProvider.nameCompany;
+        data.typeProvider = this.userProvider.typeProvider;
+        data.serviceDescription = this.userProvider.serviceDescription;
+        axios
+          .post(url, data)
+          .then((res) => {
+            console.log(res);
+            alert("Proveedor registrado correctamete");
+            this.$router.push("/");
+          })
+          .catch((err) => {
+            console.error(err);
+            alert("La persona que intenda crear ya existe");
+          });
       } else {
         this.dialog = true;
       }
